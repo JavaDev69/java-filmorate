0ec65d174c4383a232733e603e8f464af00341ac
Andrew Vilkov <alladin21@mail.ru>
2025-12-09 15:42:28 +0300

    fix status code for NotFoundByIdException

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundByIdException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundByIdException.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundByIdException extends RuntimeException {
     public NotFoundByIdException(Long id) {
         super("Сущность с id = " + id + " не найдена");
